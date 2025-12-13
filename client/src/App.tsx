9ccdaba70e33b4290c4ee4d4dfe6a01d49c84062
anu <anirudh157290@gmail.com>
2025-12-13 14:52:28 +0530

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+const App = () => {
+  return (
+    <>
+      <div className="min-h-screen">
+        <Navbar />
+        <Welcome />
+        <Services />
+        <Transactions />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
